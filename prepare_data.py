dbaa3f4ed2906d505f00e41803e6392e52e45404
erphamucsd <133862036+erphamucsd@users.noreply.github.com>
2024-11-03 13:03:07 -0800

    Update prepare_data.py
    Added some of Eric's code from his notebook.

--- prepare_data.py
+++ prepare_data.py
@@ -147,3 +147,75 @@ for experiment in experiments:
                      'pr': pr.groupby('time.year').mean('time'),
                      'pr90': pr.groupby('time.year').quantile(0.9, skipna=True)})
                 ds.to_netcdf(f"{model}_{experiment}_{member}_no_tasmin_max.nc")
+
+# Additional stuff from Eric's code. This version only creates an input file if all four variables are there.
+
+'''
+def get_data(variable, experiment, member):
+    """
+    Read a particular CMIP6 (Amon) variable from NorESM2.
+    """
+    files = glob.glob(f"/glade/collections/cmip/CMIP6/{get_MIP(experiment)}/NCC/NorESM2-LM/{experiment}/{member}/Amon/{variable}/gn/v20190815/{variable}/*.nc")
+    if files == []:
+        return (True, f"{variable} is not found.")
+    return (False, xr.open_mfdataset(files)[variable])
+
+
+def get_daily_data(variable, experiment, member):
+    """
+    Read a particular CMIP6 (day) variable from NorESM2.
+    """
+    files = glob.glob(f"/glade/collections/cmip/CMIP6/{get_MIP(experiment)}/NCC/NorESM2-LM/{experiment}/{member}/day/{variable}/gn/v20190815/{variable}/*.nc")
+    if files == []:
+        return (True, f"{variable} is not found.")
+    return (False, xr.open_mfdataset(files)[variable])
+
+for experiment in experiments:
+    # Just take three ensemble members (there are more in the COVID simulation but we don't need them all).
+    for i in range(3):
+        physics = 2 if experiment == 'ssp245-covid' else 1  # The COVID simulation uses a different physics setup. 
+        # TODO - check the differences...
+        member = f"r{i+1}i1p1f{physics}"
+        print(f"Processing {member} of {experiment}...")
+        outfile = f"{model}_{experiment}_{member}.nc"
+        if (not overwrite) and os.path.isfile(outfile):
+            print("File already exists, skipping.")
+            continue
+
+        # Call function to get variable data.
+        tasmin = get_daily_data('tasmin', experiment, member)
+        tasmax = get_daily_data('tasmax', experiment, member)
+        tas = get_data('tas', experiment, member)
+        pr = get_data('pr', experiment, member)
+
+        # Array containing booleans for which variables have data.
+        avail_arr = [tasmin[0], tasmax[0], tas[0], pr[0]]
+
+        # Give information about which variables/directory could not be found.
+        if any(avail_arr):
+            if all(avail_arr):
+                print('No variables/directory found.')
+            else:
+                for var in [tasmin, tasmax, tas, pr]:
+                    if var[0]:
+                        print(var[1])
+            print("Skipping this realisation as no data present")
+            continue
+
+        # Obtain only the data without boolean flag.
+        tasmin = tasmin[1]
+        tasmax = tasmax[1]
+        tas = tas[1]
+        pr = pr[1].persist()  # Since we need to process it twice.
+
+        # Derive additional vars.
+        dtr = tasmax-tasmin
+        pr = pr.chunk(dict(time=-1))  # Rechunk time into a single chunk.
+
+        # Process dataset.
+        ds = xr.Dataset({'diurnal_temperature_range': dtr.groupby('time.year').mean('time'),
+                         'tas': tas.groupby('time.year').mean('time'),
+                         'pr': pr.groupby('time.year').mean('time'),
+                         'pr90': pr.groupby('time.year').quantile(0.9, skipna=True)})
+        ds.to_netcdf(f"{model}_{experiment}_{member}.nc")
+'''
