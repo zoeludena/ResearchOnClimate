eab5f016abb6ca0b57919b0a4c2e1ff9a1e805f0
Ylesia Wu <130006120+ylesia-wu@users.noreply.github.com>
2024-12-03 18:49:56 -0800

    Add files via upload

--- RF_model_ESEm.py
+++ RF_model_ESEm.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
+# %%
 import os
 
 os.environ['HDF5_DISABLE_VERSION_CHECK'] = "1"
@@ -26,17 +21,12 @@ from matplotlib import colors
 
 from utils import *
 
-
-# In[12]:
-
-
+# %%
 # path to save the netcdf file
 path_output ='outputs_ssp245_prediction_ESEm.nc'
 
 
-# In[13]:
-
-
+# %%
 train_files = [ "historical", "ssp585", "ssp126", "ssp370", ]
 # Create training and testing arrays
 X, solvers = create_predictor_data(train_files)
@@ -44,14 +34,10 @@ Y = create_predictdand_data(train_files)
 
 
 
-# In[75]:
-
-
+# %%
 xr.open_dataset("inputs_historical.nc")
 
-
-# In[15]:
-
+# %%
 
 # rf_tas = rf_model(X, Y['tas'], random_state=0, bootstrap=True, max_features='auto', **{'n_estimators': 250, 'min_samples_split': 5, 'min_samples_leaf': 7,  'max_depth': 5,})
 # rf_pr = rf_model(X, Y['pr'], random_state=0, bootstrap=True, max_features='auto', **{'n_estimators': 150, 'min_samples_split': 15, 'min_samples_leaf': 8,'max_depth': 40,})
@@ -70,9 +56,7 @@ rf_pr90.train()
 rf_dtr.train()
 
 
-# In[16]:
-
-
+# %%
 ## Test on SSP245
 
 X_test = get_test_data('ssp245', solvers)
@@ -84,26 +68,20 @@ pr90_truth = Y_test["pr90"]
 dtr_truth = Y_test["diurnal_temperature_range"]
 
 
-# In[17]:
-
-
+# %%
 m_out_tas, _ = rf_tas.predict(X_test)
 m_out_pr, _ = rf_pr.predict(X_test)
 m_out_pr90, _ = rf_pr90.predict(X_test)
 m_out_dtr, _ = rf_dtr.predict(X_test)
 
 
-# In[18]:
-
+# %%
 
 xr_output = xr.Dataset(dict(tas=m_out_tas, pr=m_out_pr, pr90=m_out_pr90, diurnal_temperature_range=m_out_dtr)).assign_coords(time=m_out_tas.sample + 2014)
 #save output to netcdf 
 xr_output.to_netcdf(path_output,'w')
 
-
-# In[19]:
-
-
+# %%
 print(f"RMSE: {get_rmse(tas_truth[35:], m_out_tas[35:]).mean()}")
 print("\n")
 
@@ -115,177 +93,135 @@ print("\n")
 
 print(f"RMSE: {get_rmse(pr90_truth[35:], m_out_pr90[35:]).mean()}")
 
-
-# In[36]:
-
-
+# %%
 # Define the color normalization
 divnorm = colors.TwoSlopeNorm(vmin=-2., vcenter=0., vmax=5)
 
+# %%
+## Temperature
+proj = ccrs.PlateCarree()
+fig = plt.figure(figsize=(18, 3))
+fig.suptitle('Near-Surface Air Temperature')
 
-# In[62]:
+# Test
+plt.subplot(131, projection=proj)
+tas_truth.sel(time=2050).plot(cmap="coolwarm", vmax=3,
+                              cbar_kwargs={"label":"Temperature change / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='True')
 
+# Emulator
+plt.subplot(132, projection=proj)
+m_out_tas.sel(sample=35).plot(cmap="coolwarm", vmax=3,
+                       cbar_kwargs={"label":"Temperature change / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Random Forest')
 
-# Create a side-by-side figure
-fig, axes = plt.subplots(
-    nrows=1, ncols=2, figsize=(14, 6),
-    subplot_kw={"projection": ccrs.PlateCarree()}
-)
-
-# Plot the true projection
-with plt.style.context("dark_background"):
-    ax = axes[0]
-    tas_truth.sel(time=2050).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("True 2050")
-    ax.coastlines()
-
-# Plot the emulated result
-with plt.style.context("dark_background"):
-    ax = axes[1]
-    m_out_tas.sel(sample=35).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("Emulated 2050")
-    ax.coastlines()
-
-fig.suptitle("tas (Near-Surface Air Temperature)", fontsize=18, x=0.45)
-
-# Adjust layout for better appearance
+# Difference
+difference = tas_truth.sel(time=2050) - m_out_tas.sel(sample=35)
+plt.subplot(133, projection=proj)
+difference.plot(cmap="bwr", vmax=1,
+                cbar_kwargs={"label":"Temperature change / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Difference')
 fig.tight_layout()
 
-# Save the figure
-fig.savefig("tas_comparison.png", dpi=80, bbox_inches="tight")
+plt.savefig('rf_tas_comparison.png', dpi=250, bbox_inches="tight")
 
-plt.show()
+# %%
+## dtr
+proj = ccrs.PlateCarree()
+fig = plt.figure(figsize=(18, 3))
+fig.suptitle('Diurnal Temperature Range')
 
+# Test
+plt.subplot(131, projection=proj)
+dtr_truth.sel(time=2050).plot(cmap="coolwarm", vmax=3,
+                              cbar_kwargs={"label":"Temperature / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='True')
 
-# In[66]:
+# Emulator
+plt.subplot(132, projection=proj)
+m_out_dtr.sel(sample=35).plot(cmap="coolwarm", vmax=3,
+                       cbar_kwargs={"label":"Temperature / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Random Forest')
 
-
-# Create a side-by-side figure
-fig, axes = plt.subplots(
-    nrows=1, ncols=2, figsize=(14, 6),
-    subplot_kw={"projection": ccrs.PlateCarree()}
-)
-
-# Plot the true projection
-with plt.style.context("dark_background"):
-    ax = axes[0]
-    dtr_truth.sel(time=2050).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("True 2050")
-    ax.coastlines()
-
-# Plot the emulated result
-with plt.style.context("dark_background"):
-    ax = axes[1]
-    m_out_dtr.sel(sample=35).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("Emulated 2050")
-    ax.coastlines()
-
-fig.suptitle("dtr (Diurnal Temperature Range)", fontsize=18, x=0.45)
-
-# Adjust layout for better appearance
+# Difference
+difference = dtr_truth.sel(time=2050) - m_out_dtr.sel(sample=35)
+plt.subplot(133, projection=proj)
+difference.plot(cmap="bwr", vmax=1,
+                cbar_kwargs={"label":"Temperature / K"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Difference')
 fig.tight_layout()
 
-# Save the figure
-fig.savefig("dtr_comparison.png", dpi=80, bbox_inches="tight")
+plt.savefig('rf_dtr_comparison.png', dpi=250, bbox_inches="tight")
 
-plt.show()
+# %%
+## pr
+proj = ccrs.PlateCarree()
+fig = plt.figure(figsize=(18, 3))
+fig.suptitle('Precipitation')
 
+# Test
+plt.subplot(131, projection=proj)
+pr_truth.sel(time=2050).plot(cmap="coolwarm", vmax=3,
+                              cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='True')
 
-# In[65]:
+# Emulator
+plt.subplot(132, projection=proj)
+m_out_pr.sel(sample=35).plot(cmap="coolwarm", vmax=3,
+                       cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Random Forest')
 
-
-# Create a side-by-side figure
-fig, axes = plt.subplots(
-    nrows=1, ncols=2, figsize=(14, 6),
-    subplot_kw={"projection": ccrs.PlateCarree()}
-)
-
-# Plot the true projection
-with plt.style.context("dark_background"):
-    ax = axes[0]
-    pr_truth.sel(time=2050).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("True 2050")
-    ax.coastlines()
-
-# Plot the emulated result
-with plt.style.context("dark_background"):
-    ax = axes[1]
-    m_out_pr.sel(sample=35).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("Emulated 2050")
-    ax.coastlines()
-
-fig.suptitle("pr (Precipitation)", fontsize=18, x=0.45)
-
-# Adjust layout for better appearance
+# Difference
+difference = pr_truth.sel(time=2050) - m_out_pr.sel(sample=35)
+plt.subplot(133, projection=proj)
+difference.plot(cmap="bwr", vmax=1,
+                cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Difference')
 fig.tight_layout()
 
-# Save the figure
-fig.savefig("pr_comparison.png", dpi=80, bbox_inches="tight")
+plt.savefig('rf_pr_comparison.png', dpi=250, bbox_inches="tight")
 
-plt.show()
+# %%
+## pr90
+proj = ccrs.PlateCarree()
+fig = plt.figure(figsize=(18, 3))
+fig.suptitle('90th Percentile Precipitation')
 
+# Test
+plt.subplot(131, projection=proj)
+pr90_truth.sel(time=2050).plot(cmap="coolwarm", vmax=3,
+                              cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='True')
 
-# In[68]:
+# Emulator
+plt.subplot(132, projection=proj)
+m_out_pr90.sel(sample=35).plot(cmap="coolwarm", vmax=3,
+                       cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Random Forest')
 
-
-# Create a side-by-side figure
-fig, axes = plt.subplots(
-    nrows=1, ncols=2, figsize=(14, 6),
-    subplot_kw={"projection": ccrs.PlateCarree()}
-)
-
-# Plot the true projection
-with plt.style.context("dark_background"):
-    ax = axes[0]
-    pr90_truth.sel(time=2050).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("True 2050")
-    ax.coastlines()
-
-# Plot the emulated result
-with plt.style.context("dark_background"):
-    ax = axes[1]
-    m_out_pr90.sel(sample=35).plot(
-        ax=ax, cmap="coolwarm", norm=divnorm,
-        cbar_kwargs={"label": "Temperature change / K"}
-    )
-    ax.set_title("Emulated 2050")
-    ax.coastlines()
-
-fig.suptitle("pr90 (90th Percentile Precipitation)", fontsize=18, x=0.45)
-
-# Adjust layout for better appearance
+# Difference
+difference = pr90_truth.sel(time=2050) - m_out_pr90.sel(sample=35)
+plt.subplot(133, projection=proj)
+difference.plot(cmap="bwr", vmax=1,
+                cbar_kwargs={"label":"Precipitation (mm / day)"})
+plt.gca().coastlines()
+plt.setp(plt.gca(), title='Difference')
 fig.tight_layout()
 
-# Save the figure
-fig.savefig("pr90_comparison.png", dpi=80, bbox_inches="tight")
+plt.savefig('rf_pr90_comparison.png', dpi=250, bbox_inches="tight")
 
-plt.show()
-
-
-# In[29]:
-
-
+# %%
 from sklearn.inspection import permutation_importance
 from sklearn.metrics import make_scorer, check_scoring, mean_squared_error
 
@@ -295,28 +231,19 @@ def get_rmse_array(truth, pred):
 
     return np.sqrt(((truth - pred.reshape(-1, 96,144))**2).weighted(weights).mean(['lat', 'lon'])).data.mean()
 
-
-# In[30]:
-
-
-get_ipython().run_cell_magic('time', '', 'pr_result = permutation_importance(\n    rf_pr.model.model, X_test[35:], pr_truth.sel(time=slice(2050,None)), n_repeats=10, random_state=42, n_jobs=1, scoring=make_scorer(get_rmse_array))\n')
-
-
-# In[31]:
+# %%
+%%time
+pr_result = permutation_importance(
+    rf_pr.model.model, X_test[35:], pr_truth.sel(time=slice(2050,None)), n_repeats=10, random_state=42, n_jobs=1, scoring=make_scorer(get_rmse_array))
 
 
+# %%
 importances = rf_pr.model.model.feature_importances_
 
-
-# In[32]:
-
-
+# %%
 feature_names = list(X.columns)
 
-
-# In[88]:
-
-
+# %%
 # Feature names
 feature_names = ["CO2", "CH4", "BC_0", "BC_1", "BC_2", "BC_3", "BC_4", "SO2_0", "SO2_1", "SO2_2", "SO2_3", "SO2_4"]
 
@@ -345,9 +272,7 @@ fig.savefig("rf_feature_importances_2x2.png")
 plt.show()
 
 
-# In[122]:
-
-
+# %%
 # Feature names
 feature_names = ["CO2", "CH4", "BC_0", "BC_1", "BC_2", "BC_3", "BC_4", "SO2_0", "SO2_1", "SO2_2", "SO2_3", "SO2_4"]
 
@@ -398,4 +323,6 @@ fig.tight_layout()
 # Save and show the plot
 fig.savefig("rf_feature_importances_grouped.png")
 plt.show()
+
+
 
